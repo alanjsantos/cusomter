fe5a843befd486c4993902d7cb3c1bc67f005a0b
Alan Santos <alanjosedossantos011@gmail.com>
2022-01-18 21:06:59 -0300

    confiuracao docker

---
+++ customer/src/main/java/com/api/customer/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.api.customer.repository;
+
+import com.api.customer.model.Endereco;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends MongoRepository<Endereco, String> {
+}
